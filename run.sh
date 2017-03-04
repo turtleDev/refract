f48b852ceccdf6164797bc59b54490cc0d79ab0a
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-03-04 20:00:41 +0530

    add runner script

---
+++ run.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+source ./config.sh
+
+trap exit SIGINT SIGTERM
+
+log_err() {
+    echo '[Error]' $@
+}
+
+exists() {
+    which $1 &>/dev/null
+}
+
+
+try() {
+    exists $1
+    if [[ $? != 0 ]]; then
+        log_err "$1 not found. try running $0 --install-deps"
+    else
+        exec $@ 
+        return $?
+    fi
+}
+
+install_node_packages() {
+    exists npm
+    NPM=$?
+    
+    exists yarn
+    YARN=$?
+
+    if [[ $NPM  == '0' ]]; then
+        exec npm install
+    elif [[ $YARN == '0' ]]; then
+        exec yarn install
+    else
+        log_err "can't find any node package managers"
+    fi
+}
+
+
+
+for i in "$@"
+do
+case $i in 
+    --collect)
+        cd scraper/slackarchive
+        try scrapy crawl archive
+        exit
+        ;;
+    --install-deps)
+        THIS=$PWD
+        cd app/
+        install_node_packages
+        cd $THIS
+        cd scraper/slackarchive
+        try pip install -r requirements.txt
+        exit
+        ;;
+    --main)
+        cd app/
+        try gulp
+        exit
+        ;;
+esac
+done
+
+echo "usage: $0 [--collect | --install-deps | --main ]"
